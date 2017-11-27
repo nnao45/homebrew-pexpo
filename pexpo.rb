5e63dee76fb28eac23b828a429a1a1323f475282
nnao45 <n4sekai5y@gmail.com>
2017-11-28 02:02:02 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Pexpo < Formula
   homepage "https://github.com/nnao45/pexpo"
-  url "https://github.com/nnao45/pexpo/releases/download/1.30/pexpo-1.30-darwin-amd64.zip"
+  url "https://github.com/nnao45/pexpo/archive/1.30.tar.gz"
   sha256 "808a2308d7075b2131c11b9b24e8d2f3e9daf2e99fba043551036ec3d4c10226"
   head "https://github.com/nnao45/pexpo.git"
   version "1.30"
@@ -12,6 +12,7 @@ class Pexpo < Formula
   
   def install
     ENV["GOPATH"] = buildpath
+    cd buildpath/"src/github.com/peco/peco" do
     
     # Install Go dependencies
     system "go", "get", "github.com/dariubs/percent"
