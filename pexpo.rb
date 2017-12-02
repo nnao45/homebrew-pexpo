5dbe3d9df0c6ecca3479a5d42fc2ce58d7738ba3
nnao45 <n4sekai5y@gmail.com>
2017-12-02 16:13:19 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -3,7 +3,7 @@ require "formula"
 class Pexpo < Formula
   homepage "https://github.com/nnao45/pexpo"
   url "https://github.com/nnao45/pexpo/archive/1.33.tar.gz"
-  sha256 "764f08c054d8c2b8f5b0f895917afb018b640b70cce6210a1141e998843548fd"
+  sha256 "9ff55d9173cd98c8e397658b9a314b4d815d5158be112c558bbbaa2a80fdbc3d"
   head "https://github.com/nnao45/pexpo.git"
   version "1.33"
 
