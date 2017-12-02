72140c02b17f887b9379b4e40ad5893bed2ed39d
nnao45 <n4sekai5y@gmail.com>
2017-12-02 16:10:39 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Pexpo < Formula
   homepage "https://github.com/nnao45/pexpo"
-  url "https://github.com/nnao45/pexpo/archive/1.32.tar.gz"
+  url "https://github.com/nnao45/pexpo/archive/1.33.tar.gz"
   sha256 "764f08c054d8c2b8f5b0f895917afb018b640b70cce6210a1141e998843548fd"
   head "https://github.com/nnao45/pexpo.git"
-  version "1.32"
+  version "1.33"
 
   depends_on "go" => :build
   depends_on :hg => :build
