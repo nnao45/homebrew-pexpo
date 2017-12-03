649550c56117220be49902dc1a2ee89010397723
nnao45 <n4sekai5y@gmail.com>
2017-12-03 17:13:57 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -28,11 +28,11 @@ class Pexpo < Formula
     #system "go", "get", "github.com/tatsushid/go-fastping"
 	  
     # Build and install termshare
-    #cd buildpath/"src/github.com/nnao45/pexpo" do
+    cd buildpath/"src/github.com/nnao45/pexpo" do
     #cd "src/github.com/nnao45/pexpo" do
       system "glide", "install"
       system "go", "build", "-o", "pexpo"
       bin.install "pexpo"
     end
   end
-#end
+end
