491858ecf4d2a973a5ac1aac6d6c3d1c32ab0fe3
nnao45 <n4sekai5y@gmail.com>
2017-12-03 16:13:21 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -12,15 +12,18 @@ class Pexpo < Formula
   
   def install
     ENV["GOPATH"] = buildpath
+    glidepath = buildpath/"src/github.com/nnao45/pexpo"
+    glidepath.install buildpath.children
     
     # Install Go dependencies
-    system "go", "get", "github.com/dariubs/percent"
-    system "go", "get", "github.com/mattn/go-runewidth"
-    system "go", "get", "github.com/nsf/termbox-go"
-    system "go", "get", "github.com/tatsushid/go-fastping"
+    #system "go", "get", "github.com/dariubs/percent"
+    #system "go", "get", "github.com/mattn/go-runewidth"
+    #system "go", "get", "github.com/nsf/termbox-go"
+    #system "go", "get", "github.com/tatsushid/go-fastping"
 	  
     # Build and install termshare
-    system "go", "build", "-o", "pexpo"
-    bin.install "pexpo"
+    cd glidepath do
+      system "go", "build", "-o", "pexpo"
+      bin.install "pexpo"
   end
 end
