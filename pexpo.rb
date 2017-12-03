9c7b37036dbe18ab7a61015640c805c10d93d999
nnao45 <n4sekai5y@gmail.com>
2017-12-03 15:59:24 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -14,6 +14,7 @@ class Pexpo < Formula
     ENV["GOPATH"] = buildpath
     
     # Install Go dependencies
+    system "glide", "up"
     #system "go", "get", "github.com/dariubs/percent"
     #system "go", "get", "github.com/mattn/go-runewidth"
     #system "go", "get", "github.com/nsf/termbox-go"
