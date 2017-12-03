b788573e7dbf2873d9354b45282392560dbb1eca
nnao45 <n4sekai5y@gmail.com>
2017-12-03 19:07:05 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -3,22 +3,29 @@ require "formula"
 class Pexpo < Formula
   desc "Terminal sending ping tool written in Go."
   homepage "https://github.com/nnao45/pexpo"
-  url "https://github.com/nnao45/pexpo/archive/1.35.tar.gz"
+  url "https://github.com/nnao45/pexpo/archive/1.36.tar.gz"
   sha256 "4de53773d961b821b65e8acb5523e62c65aa845580223bc9e96d561c7cc2e15a"
   head "https://github.com/nnao45/pexpo.git"
-  version "1.35"
+  version "1.36"
 
   depends_on "go" => :build
   depends_on "glide" => :build
   
   def install
     ENV["GOPATH"] = buildpath
-    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-    (buildpath/"src/github.com/nnao45/pexpo").install buildpath.children
-    cd "src/github.com/nnao45/pexpo" do
-      system "glide", "install"
-      system "go", "build", "-o", "pexpo"
-      bin.install "pexpo"
+    ENV["GLIDE_HOME"] = buildpath/"glide_home"
+    (buildpath/"src/github.com/nnao45").mkpath
+    ln_s buildpath, buildpath/"src/github.com/nnao45/pexpo"
+    
+    # Install Go dependencies
+    #system "go", "get", "github.com/dariubs/percent"
+    #system "go", "get", "github.com/mattn/go-runewidth"
+    #system "go", "get", "github.com/nsf/termbox-go"
+    #system "go", "get", "github.com/tatsushid/go-fastping"
+	  
+    # Build and install termshare
+    system "glide", "install"
+    system "go", "build", "-o", "pexpo"
+    bin.install "pexpo"
     end
   end
-end
