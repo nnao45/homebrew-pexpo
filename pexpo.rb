5bc9b7ae56474eba76e5e40ae69a0e411f653b48
nnao45 <n4sekai5y@gmail.com>
2017-12-09 21:55:49 +0900

    Update pexpo.rb

--- pexpo.rb
+++ pexpo.rb
@@ -3,10 +3,10 @@ require "formula"
 class Pexpo < Formula
   desc "Terminal sending ping tool written in Go."
   homepage "https://github.com/nnao45/pexpo"
-  url "https://github.com/nnao45/pexpo/archive/1.38.tar.gz"
+  url "https://github.com/nnao45/pexpo/archive/1.39.tar.gz"
   sha256 "7e16f7e8655da0cbed4d079c34096132c02b33a6fcb585bd4240001cd217d620"
   head "https://github.com/nnao45/pexpo.git"
-  version "1.38"
+  version "1.39"
 
   depends_on "go" => :build
   depends_on "glide" => :build
